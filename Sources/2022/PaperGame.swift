fbb263327a57ee9bf4f0184ea7b299a325cf8efe
tomieq <tomieq@wp.pl>
2022-12-14 07:55:26 +0100

    Published PaperGame enum

---
+++ Sources/2022/PaperGame.swift
@@ -0,0 +1,20 @@
+//
+//  PaperGame.swift
+//
+//
+//  Created by Tomasz on 02/12/2022.
+//
+
+import Foundation
+
+enum PaperGame: Int {
+    case rock = 1
+    case paper = 2
+    case scissors = 3
+}
+
+enum PaperGameResult: Int {
+    case win = 6
+    case lost = 0
+    case draw = 3
+}
